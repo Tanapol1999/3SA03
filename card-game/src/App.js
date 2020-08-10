b3d50197d645a3a1b4f9d671e245e5ec7487c191
TanapolFirst <6110110178@psu.ac.th>
2020-08-10 16:21:19 +0700

    Commit1

---
+++ card-game/src/App.js
@@ -0,0 +1,13 @@
+import CharacterCard from './CharacterCard';
+
+function App() {
+    return ( <
+        div >
+        <
+        CharacterCard value = "h" / >
+        <
+        CharacterCard value = "i" / >
+        <
+        /div>
+    );
+}
